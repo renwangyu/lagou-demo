5fd70a05d02c060e53e1e0d95055c42fe6009e03
renwangyu <renwangyu1106@gmail.com>
2022-08-23 22:09:08 +0800

    feat: 增加webrtc的demo

---
+++ my-video-room/public/client.js
@@ -0,0 +1,64 @@
+const socket = io('/');
+const videoGrid = document.getElementById('video-grid');
+
+const myPeer = new Peer(undefined, {
+  host: '/',
+  port: '3001',
+});
+
+const peers = {};
+
+const myVideo = document.createElement('video');
+myVideo.muted = true;
+
+navigator.mediaDevices.getUserMedia({
+  video: true,
+  audio: true,
+}).then(stream => {
+  addVideoStream(myVideo, stream);
+
+  myPeer.on('call', call => {
+    call.answer(stream);
+    const video = document.createElement('video');
+    video.muted = true;
+    call.on('stream', userVideoStream => {
+      addVideoStream(video, userVideoStream);
+    });
+  });
+
+  socket.on('user-connected', userId => {
+    console.log('user connected: ' + userId);
+    connectToNewUser(userId, stream);
+  });
+})
+
+socket.on('user-disconnected', userId => {
+  if (peers[userId]) {
+    peers[userId].close();
+  }
+});
+
+myPeer.on('open', id => {
+  socket.emit('join-room', ROOM_ID, id);
+});
+
+function addVideoStream(video, stream) {
+  video.srcObject = stream;
+  video.addEventListener('loadedmetadata', () => {
+    video.play();
+  });
+  videoGrid.append(video);
+}
+
+function connectToNewUser(userId, stream) {
+  const call = myPeer.call(userId, stream);
+  const video = document.createElement('video');
+  video.muted = true;
+  call.on('stream', userVideoStream => {
+    addVideoStream(video, userVideoStream)
+  });
+  call.on('close', () => {
+    video.remove();
+  });
+  peers[userId] = call;
+}
